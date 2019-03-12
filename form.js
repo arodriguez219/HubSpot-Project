07e88e547eccfb73c2e79b3dd8e9385d22cf401c
arodriguez219 <36651527+arodriguez219@users.noreply.github.com>
2019-03-11 20:28:37 -0500

    Add files via upload

---
+++ form.js
@@ -0,0 +1,20 @@
+//variable to hold form data
+var form;
+//the URL to the API
+var url = 'https://api.hubapi.com/integrations/v1/<application id>/timeline/event-types?hapikey=612d9033-117b-42bb-8110-23141bb9f97e';
+
+//var name;
+
+function setup () {
+//button element on the page
+//var button = select ('#submit');
+//when the button is pressed, run the sendContact function
+hbspt.forms.create({
+portalId: "5578497",
+formId: "d122bb35-8854-430b-8411-82e7556978ac"
+onFormSubmit {
+
+}
+}
+
+}
